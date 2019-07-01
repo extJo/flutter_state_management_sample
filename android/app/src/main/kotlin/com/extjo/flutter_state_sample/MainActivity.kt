14eadb7bc33dccfdbca643eca7b60f2bc143b052
extJo <josungyun6518@gmail.com>
2019-07-01 20:00:22 +0900

    init

---
+++ android/app/src/main/kotlin/com/extjo/flutter_state_sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.extjo.flutter_state_sample
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
